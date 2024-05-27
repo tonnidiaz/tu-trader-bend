9fc23a7a57148105a7bad885d3dd985616d92170
tonnidiaz <tonnidiazed@gmail.com>
2024-05-27 08:44:26 +0200

    with aps_scheduler

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from flask_apscheduler import APScheduler
+import gunicorn
 
 app = Flask(__name__)
+scheduler = APScheduler()
+
+class Config:
+    SCHEDULER_API_ENABLED = True
+app.config.from_object(Config)
 
 @app.get('/')
 def index():
     return "HELLO WORLD"
 
+cnt = 0
+job_id = "job0"
+
+@scheduler.task("interval", id=job_id, seconds=1, misfire_grace_time=900)
+def job0():
+    global cnt
+    print(f"CNT: {cnt}")
+    cnt += 1
+
+scheduler.start()
 if __name__ == "__main__":
     app.run()
+
+print(gunicorn)
